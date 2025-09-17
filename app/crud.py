3be88080cc40c029ea72fba6a6b9c60413ecc9e2
Fatima Coronado <fatimacoronado2101@gmail.com>
2025-09-16 23:55:49 -0600

    integrate password hashing

--- app/crud.py
+++ app/crud.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 from app.models import User
-from app.schemas import UserCreate, RoleEnum
+from app.schemas import UserCreate, UserLogin
+from app.security import hash_password, verify_password
 
 def get_user_by_email(db: Session, email: str):
     return db.query(User).filter(User.email == email).first()
@@ -12,10 +13,19 @@ def create_user(db: Session, user: UserCreate):
     db_user = User(
         email=user.email,
         username=user.username,
-        hashed_password=user.password, 
+        hashed_password=hash_password(user.password),
         role=user.role.value
     )
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
+
+
+def authenticate_user(db: Session, user_login: UserLogin):
+    user = get_user_by_email(db, user_login.email)
+    if not user:
+        return False
+    if not verify_password(user_login.password, user.hashed_password):
+        return False
+    return user
--- app/main.py
+++ app/main.py
@@ -3,6 +3,10 @@ from sqlalchemy.orm import Session
 from sqlalchemy import text
 from app.database import get_db
 from app.models import User 
+from app.schemas import UserCreate, UserResponse, UserLogin  # ← Nuevos imports
+from app.crud import create_user, authenticate_user  # ← Nuevos imports
+from app.security import hash_password, verify_password  # ← Nuevos imports
+
 
 app = FastAPI(title="Coworking Reservations", version="1.0.0")
 
@@ -26,3 +30,29 @@ def test_db(db: Session = Depends(get_db)):
 def count_users(db: Session = Depends(get_db)):
     count = db.query(User).count()
     return {"users_count": count, "table": "users table working!"}
+    
+# ENDPOINTS DE PRUEBA
+""" @app.post("/test-hash")
+def test_password_hashing():
+    password = "changedpsswd"
+    hashed = hash_password(password)
+    verification = verify_password(password, hashed)
+    return {
+        "original": password,
+        "hashed": hashed,
+        "verification_correct": verification,
+        "verification_wrong": verify_password("password_incorrecta", hashed)
+    }
+
+@app.post("/test-user", response_model=UserResponse)
+def create_test_user(user: UserCreate, db: Session = Depends(get_db)):
+    return create_user(db=db, user=user)
+
+@app.post("/test-auth")
+def test_authentication(user_login: UserLogin, db: Session = Depends(get_db)):
+    user = authenticate_user(db, user_login)
+    if user:
+        return {"authentication": "success", "user_id": user.id, "username": user.username}
+    return {"authentication": "failed"}
+
+ """
