41c95337764bdc766b1d9343d2fbbf8ad4dfa666
fatimacm <fatimacoronado2101@gmail.com>
2026-06-14 14:46:49 -0600

    add CORS configuration

--- app/main.py
+++ app/main.py
@@ -11,9 +11,20 @@ from datetime import datetime
 from app.dependencies import get_current_user 
 from fastapi.security import OAuth2PasswordRequestForm
 from typing import List
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(title="Coworking Reservations", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
